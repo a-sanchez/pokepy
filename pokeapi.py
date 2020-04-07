f8f3259b5182041b30f2658dc9f2ea87c6a8a29c
Angela Sanchez <a-sanchez@uadec.edu.mx>
2020-04-06 19:08:10 -0500

    final

--- pokeapi.py
+++ pokeapi.py
@@ -18,7 +18,7 @@ class PokeAPI(object):
             'height': data.get('height'),
             'weight': data.get('weight'),
             'image': data.get('sprites', {}).get('front_default'),
-            'stats': data.get('stats',{}).get ('0'),
+            'stats': ,[i.get('stats',{}).get('base_stat')for i in data.get('stats')]
             'base_experiencie': data.get('base_experiencie'),
-            'types': data.get('types',{}).get('0')
+            'types': [i.get('type',{}).get('name')for i in data.get('types')]
 }
